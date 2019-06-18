bf303274f77bf4d7a11d3637d388e3921a06142b
lbCyber <paul@lintbox.com>
2019-06-18 12:12:53 -0400

    Making final touches to 1.0 build

--- js/main.js
+++ js/main.js
@@ -6,27 +6,9 @@ $(function () {
           behavior: `smooth`
         })
       })
-      // },
-      // navToggle: () => {		
-      //   const bar = {
-      //     1: "bar1",
-      //     2: "bar2",
-      //     3: "bar3",
-      //     4: "mainNav",
-      //     5: "mobileNavButton"
-      //   }
-      //   for (i = 1; i < 6; i++) {
-      //     // if (window.innerWidth <= 768) {
-      //       $(`${bar[i]}`).toggleClass(`${bar[i]}Click`);
-      //     // }
-      //   }
     }
   }
-  // $('.mobileElement').on('click', () => {
-  //   assets.navToggle()
-  // })
   assets.scroller($('.aboutButton'), $('#about'))
   assets.scroller($('.skillsButton'), $('#skills'))
   assets.scroller($('.portfolioButton'), $('#portfolio'))
 })
-
