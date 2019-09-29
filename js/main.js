389d4ac5b4695a2959c7a296f494b8e8f1f527d8
lbCyber <paul@lintbox.com>
2019-09-28 23:31:57 -0400

    Made a ton of refinements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   const assets = {
     scroller: (o, t) => {
       o.on(`click`, () => {
@@ -12,10 +12,35 @@ $(function () {
   assets.scroller($('.skillsButton'), $('#skills'))
   assets.scroller($('.portfolioButton'), $('#portfolio'))
   assets.scroller($('.upArrow'), $('#home'))
-  $(document).scroll(() => {
-    $('.mainNav').toggleClass('scrollDown', $(this).scrollTop() > $('.mainNav').height());
-  });
+    // $(document).scroll(() => {
+    //   $('.mainNav').toggleClass('scrollDown', $(this).scrollTop() > $('.mainNav').height());
+    // });
   $(document).scroll(() => {
     $('.navText').toggleClass('scrollDownText', $(this).scrollTop() > $('.navText').height());
+    $('.mainNav').toggleClass('scrollDownNav', $(this).scrollTop() > $('.mainNav').height());
   });
+
+  const navToggle = () => {
+    if (window.innerWidth < 769) {
+      const bar = [
+        "bar1",
+        "bar2",
+        "bar3",
+        "mainNav",
+        "mobileNavButton"
+      ]
+      for (i = 0; i < 5; i++) {
+        $(`.${bar[i]}`).toggleClass(`${bar[i]}Click`)
+      }
+    }
+  }
+
+  $('.mobileNavButton').on('click', () => {
+    console.log('blep')
+    navToggle()
+  })
+  $('.mainNavItem').on('click', () => {
+    console.log('blep')
+    navToggle()
+  })
 });
